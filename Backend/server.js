6f52f26efab72d5ad19e4f3a201cac42bae0bcbd
Surafel Mulugeta <suramuller405@gmail.com>
2026-08-13 01:36:34 -0700

    connect backend and frontend via CORS

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from "cors";
+import cookieParser from 'cookie-parser';
 import { connectDB } from './config/db.js';
 
 import authRoute from './Routes/authRoute.js';
@@ -13,7 +15,16 @@ import adminRoute from './Routes/adminRoute.js';
 const app = express();
 const PORT = process.env.PORT || 5002;
 
+//To connect backend to frontend  
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/booking", bookingRoute);
@@ -23,8 +34,15 @@ app.use("/api/review", reviewRoute);
 app.use("/api/admin", adminRoute);
 
 
+// Centralized error handler
+app.use((err, req, res, next) => {
+  console.error('Unhandled error: ', err.stack);
+  res.status(500).json({ message: 'Internal server error' });
+});
+
+//It will run the server after connect to database
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+        console.log(`PhotoSasha Server is running on port ${PORT}`);
     })
 });
